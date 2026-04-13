a00dc1d0a433aaa86644cd61af1326fd152ef873
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2026-04-13 12:49:35 +0300

    Update Design code.sv

--- Design code.sv
+++ Design code.sv
@@ -30,23 +30,43 @@ module req_ack (
     end
 
 endmodule
-    // בדיקה שהדאטה לא משתנה בזמן Handshake
-    property p_stable_data;
-        @(posedge clk) (req && !ack) |=> $stable(data_in) throughout (ack [->1]);
-    endproperty
 
-    assert_data_stability: assert property (p_stable_data)
-        else $error("ERROR: Data changed while waiting for ACK!");
+Interface req_ack (input bit clk); 
 
-    // בדיקה שה-ACK לא עולה בפתאומיות בלי REQ
-    property p_no_spurious_ack;
-        @(posedge clk) $rose(ack) |-> req;
-    endproperty
+Logic clk; //positive edge clk 
+Logic reset_n; // A-Sychronic reset, active low 
+Logic req; // active high 
+Logic ack; // active high 
+Logic [7:0] data; // 2^8 = 0-255 
 
-    assert_ack_valid: assert property (p_no_spurious_ack)
-        else $error("ERROR: ACK rose without a valid REQ!");
+Property p_data_stability
+@(posedge clk) disable iff (!reset_n || !assertions_en) // after clk, disable reset only if
+(req && !ack) |=> $stable(data) throughout (ack [->1]);
+endproperty 
 
-endinterface
+Property p_no_spurious_ack;
+@(posedge clk) disable iff (!reset_n || !assertions_en) // after clk, disable this test only if reset is active
+$rose(ack) -> req;
+Endproperty
+
+Property p_req_persistence; 
+@(posedge clk) disable iff (!reset_n || !assertions_en)
+(req && !ack) | => req;
+Endpropery
+
+// Assertion Directives 
+
+Assert_data_stability: assert property (p_data_stability) // condition that must be in the system.
+Else $error (“[SVA ERROR] DATA toggled while waiting for ACK”);
+
+Assert_act_vaild: assert property (p_no_spurious_ack);
+Else $error “([SVA ERROR] ACK rose without a valid REQ!”);
+
+
+Assert_req_persistence assert property (p_req_persistence);
+Else $error “([SVA ERROR] REQ dropped before ACK was received!”);
+
+Endinterface
    
     
     
