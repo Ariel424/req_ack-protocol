f8b01e7feda2a2958c553033c5cb1f844bc10b9b
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2026-04-16 11:45:52 +0300

    Update UVM_Classes.sv

--- UVM_Classes.sv
+++ UVM_Classes.sv
@@ -36,7 +36,9 @@ endclass
 
 class my_sequencer extends uvm_sequencer #(my_transaction);
   `uvm_component_utils(my_sequencer)
-  function new(string name, uvm_component parent); super.new(name, parent); endfunction
+  function new(string name, uvm_component parent); 
+  super.new(name, parent); 
+  endfunction
 endclass
 
 // -------------------------------------------------------------------------
